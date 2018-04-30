ab30700a821be80332fd8af8b9953050aa2288a6
jmoj95 <josemurilodeoliveirajr@gmail.com>
2018-04-30 04:42:42 -0300

    Some aesthetic tweaks.

--- config.h
+++ config.h
@@ -3,9 +3,9 @@ static const char *user  = "murilo";
 static const char *group = "murilo";
 
 static const char *colorname[NUMCOLS] = {
-	[INIT] =   "#000000",   /* after initialization */
-	[INPUT] =  "#565575",   /* during input */
-	[FAILED] = "#ff8080",   /* wrong password */
+	[INIT] =   "#101010",   /* after initialization */
+	[INPUT] =  "#356579",   /* during input */
+	[FAILED] = "#b91e2e",   /* wrong password */
 };
 
 /* treat a cleared input like a wrong password (color) */
